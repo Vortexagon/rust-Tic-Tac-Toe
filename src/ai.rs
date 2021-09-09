9c3a2c1dd1f58ecd2f908567f8f22ea40eb12b63
RealVortexed <RealVortexed@gmail.com>
2021-09-09 22:03:09 +0100

    Add alpha-beta pruning

--- src/ai.rs
+++ src/ai.rs
@@ -1,19 +1,25 @@
-use super::board::{Board, State, Mark};
-
+use super::board::{Board, Mark, State};
+use std::cmp;
+use std::time::Instant;
 pub struct OptimalAi;
 
 impl OptimalAi {
     fn opposite(mark: Mark) -> Mark {
-        if mark == Mark::Cross { Mark::Nought } else { Mark::Cross }
+        if mark == Mark::Cross {
+            Mark::Nought
+        } else {
+            Mark::Cross
+        }
     }
 
     pub fn make_move(board: &mut Board, ai_mark: Mark) {
+        let now = Instant::now();
         let mut best_score = -999;
         let mut best_index = 0;
 
         for index in board.get_free_cells() {
             board.set_cell(ai_mark, index);
-            let score = OptimalAi::minimax(board, false, ai_mark);
+            let score = OptimalAi::minimax(board, -999, 999, false, ai_mark);
             board.clear_cell(index);
 
             if score > best_score {
@@ -23,21 +29,24 @@ impl OptimalAi {
         }
 
         board.set_cell(ai_mark, best_index);
+        println!(
+            "Took {} milliseconds taken to choose move!",
+            now.elapsed().as_millis()
+        );
     }
 
-    pub fn minimax(board: &mut Board, maximising: bool, ai_mark: Mark) -> i32 {
+    pub fn minimax(
+        board: &mut Board,
+        mut alpha: i32,
+        mut beta: i32,
+        maximising: bool,
+        ai_mark: Mark,
+    ) -> i32 {
         let state = board.get_state();
         let opp_mark = OptimalAi::opposite(ai_mark);
 
         match state {
-            State::Win(mark) => {
-                if mark == ai_mark {
-                    return 1;
-                }
-                else {
-                    return -1;
-                }
-            },
+            State::Win(mark) => return if mark == ai_mark { 1 } else { -1 },
             State::Draw => return 0,
             State::Unfinished => (),
         };
@@ -45,12 +54,26 @@ impl OptimalAi {
         let mut final_score = if maximising { -999 } else { 999 };
 
         for index in board.get_free_cells() {
-            board.set_cell(if maximising {ai_mark} else {opp_mark}, index);
-            let score = OptimalAi::minimax(board, !maximising, ai_mark);
+            board.set_cell(if maximising { ai_mark } else { opp_mark }, index);
+
+            let score = OptimalAi::minimax(board, alpha, beta, !maximising, ai_mark);
+
             board.clear_cell(index);
 
-            if maximising && score > final_score || !maximising && score < final_score {
-                final_score = score;
+            if maximising {
+                final_score = cmp::max(final_score, score);
+                alpha = cmp::max(alpha, final_score);
+
+                if beta <= final_score {
+                    break;
+                }
+            } else {
+                final_score = cmp::min(final_score, score);
+                beta = cmp::min(beta, final_score);
+
+                if alpha >= final_score {
+                    break;
+                }
             }
         }
         final_score
